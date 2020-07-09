97b9a1535df8036b77bae45ca533ad5017e862d1
bogyung99 <bogung1208@naver.com>
2020-07-09 22:41:31 +0900

    first commit

---
+++ Single Number.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Solution {
+    public int singleNumber(int[] a) {
+        int[] b = new int[100];
+        int s = a[0];
+        int t = 0;
+
+        for(int i = -49; i < 50; i++){
+            b[t] = i;
+            t++;
+        }
+        
+        for(int i = 0; i < 20; i++){
+            int n = 0;
+                
+            for(int j = 0; j < a.length; j++){
+                if(b[i] == a[j]){
+                    n++;
+                }
+            }
+            
+            if(n == 1){
+                s = b[i];
+            }
+        }
+
+        return s;
+    }
+}
