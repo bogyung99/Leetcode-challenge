f16508534ce9300665684128b27535eb7915a914
bogyung99 <bogung1208@naver.com>
2020-07-10 16:22:33 +0900

    HashMap

--- Single Number - HashMap.java
+++ Single Number - HashMap.java
@@ -6,15 +6,15 @@ class Solution {
         HashMap<Integer, Integer> ht = new HashMap<>();
         
         for(int i : nums){
-            ht.put(i, ht.getOrDefault(i, 0) + 1);
+            ht.put(i, ht.getOrDefault(i, 0) + 1); // 찾는 키가 존재한다면 찾는 키의 값을 반환하고 없다면 기본 값을 반환한다.
         }
         
         for(int i : nums){
-            if(ht.get(i) == 1)
+            if(ht.get(i) == 1) // 중복이 아닌 키는 값이 1으로 저장되있음
                 return i;
         }
         
-        return 0;
+        return 0; // 중복되지 않은 키가 하나 존재하기 때문에 기본 리턴
     }
 }
 }
