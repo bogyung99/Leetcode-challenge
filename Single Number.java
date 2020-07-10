d1d34dffda36cb6c8f79af976a34c1ab40656a20
bogyung99 <bogung1208@naver.com>
2020-07-10 15:43:30 +0900

    final

--- Single Number.java
+++ Single Number.java
@@ -1,8 +1,28 @@
 class Solution {
-  public int singleNumber(int[] nums) {
-    int a = 0;
-    for (int i : nums) {
-      a ^= i;
+    public int singleNumber(int[] a) {
+        int[] b = new int[100];
+        int s = a[0];
+        int t = 0;
+
+        for(int i = -49; i < 50; i++){
+            b[t] = i;
+            t++;
+        }
+        
+        for(int i = 0; i < 20; i++){
+            int n = 0;
+                
+            for(int j = 0; j < a.length; j++){
+                if(b[i] == a[j]){
+                    n++;
+                }
+            }
+            
+            if(n == 1){
+                s = b[i];
+            }
+        }
+
+        return s;
     }
-    return a;
-  }
+}
