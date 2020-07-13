ff976e5a4fc4061de80eea7167b85a90c0fb6dd0
bogyung99 <bogung1208@naver.com>
2020-07-14 00:37:42 +0900

    HashMap

--- Happy Number/Happy Number.java
+++ Happy Number/Happy Number.java
@@ -3,8 +3,7 @@ import java.util.Map;
 
 class Solution {
     public boolean isHappy(int n) {
-        HashMap<Integer, Integer> ht1 = new HashMap<>();
-        HashMap<Integer, Integer> ht2 = new HashMap<>();
+        HashMap<Integer, Integer> ht = new HashMap<>();
         
         int m = 0;
         int cp = 0;
@@ -15,16 +14,15 @@ class Solution {
             cp = n;
             
             for(int i = 0; n > 0; i++){
-                ht1.put(i, n % 10); // 각각 숫자 배열
+                ht.put(i, n % 10); // 각각 숫자 배열
                 sum++; // 자릿수
                 n /= 10;
             }
         
             for(int i = 0; i < sum; i++){
-                s += ht1.get(i) * ht1.get(i);
+                s += ht.get(i) * ht.get(i);
             }
         
-            ht2.put(m, s);
             n = s;
             
             m++;
